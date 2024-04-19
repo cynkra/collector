406cbb06e241e2355db4c9427b9e82041bfabb6c
Antoine Fabri <antoine.fabri@gmail.com>
2024-04-19 12:52:07 +0200

    add exception for global env

--- R/collector.R
+++ R/collector.R
@@ -122,8 +122,8 @@ collect_and_run <- function() {
 #   value they have at evaluation time
 env_clone_lazy <- function(env) {
   # we stop the recursion when we find a special env, defined by having a name
-  if (env_name(env) != "") return(env)
 
+  if (!env_name(env) %in% c("", "global")) return(env)
   parent_clone <- env_clone_lazy(env_parent(env))
   clone <- rlang::env_clone(env, parent = parent_clone)
   # don't touch dots or bindings that are already lazy
@@ -140,7 +140,7 @@ env_clone_lazy <- function(env) {
 
 # drop lazy bindings, since these were not used
 env_cleanup <- function(env) {
-  if (env_name(env) != "") return(env)
+  if (!env_name(env) %in% c("", "global")) return(env)
   env_cleanup(env_parent(env))
   lazy_lgl <- env_binding_are_lazy(env)
   rm(list = names(lazy_lgl)[lazy_lgl], envir = env)
